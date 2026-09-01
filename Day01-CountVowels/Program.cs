cdd1981237dc4f32c984a6f7ce34b824ea3474d6
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-01 08:39:10 +0530

    Sep-Day01 Basics c# commit

---
+++ Day01-CountVowels/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowels
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String");
+            string? userInput = Console.ReadLine();
+            string? LowercaseInput = userInput?.ToLower();
+
+            int vowelCount = 0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };    
+
+            for (int i = 0;i < LowercaseInput.Length; i++)
+            {
+                if (vowels.Contains(LowercaseInput[i]))
+                {
+                    vowelCount++;
+                }
+            }
+            Console.WriteLine($"The number of vowels in the string is: {vowelCount} in the word : {LowercaseInput}");
+
+        }
+    }
+}
---
+++ Day01-FindMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FindMax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 5, 7, 3, 12, 9 };
+            int max = num[0];
+
+            for (int i = 1; i < num.Length; i++)
+            {
+                //Console.WriteLine(num[i]);
+                if (num[i] > max)
+                {
+                    max = num[i];
+                }
+            }
+            Console.WriteLine("The maximum number is: " + max);
+        }
+    }
+}
