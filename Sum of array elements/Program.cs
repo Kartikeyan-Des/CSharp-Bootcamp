9b6ce6bc55d9b5a03d65aa182779e7c766d6c54c
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-08 04:49:30 +0530

    Day-02 Sum of Arrays

---
+++ Sum of array elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 4, 6, 8, 10 };
+
+            int sum = 0;
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                sum += num[i];
+            }
+            Console.WriteLine ($"The Total sum of Array is :  {sum}");
+        }
+    }
+}
