f030d4ed8c1e0d967c068c198683845f55253f1d
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-07 09:10:09 +0530

    Sep 7

---
+++ Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number Factorial :  ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            long fact = 1;
+
+            while(num > 1)
+            {
+                fact = fact * num;
+                num--;  
+            }
+
+            Console.WriteLine("Factorial is : " + fact);
+
+
+        }
+    }
+}
