f8e939c0c055f7d9a175348fbb5f82a11129fbf5
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-08 07:40:51 +0530

    Day 02- count digits in a number

---
+++ Digit Counting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number to find no of digits it hold");
+            int num =Convert.ToInt32(Console.ReadLine());
+
+            int digits = 0;
+
+
+            while(num > 0)
+            {
+                num = num / 10;
+
+                digits++;   
+            }
+
+            Console.WriteLine($"The Digit for is : {digits}");
+        }
+    }
+}
