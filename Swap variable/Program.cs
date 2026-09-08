1f408fd32f940bfd99c36f7a8bfc0cd721bf9356
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-08 06:00:48 +0530

    Day 02 - Swap Variable

---
+++ Swap variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace swapVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+
+            a = a + b;
+
+            b = a - b;
+
+            a = a - b;
+
+            
+            Console.WriteLine($"val of a: {a}");
+            Console.WriteLine($"val of b: {b}");
+        }
+    }
+}
