486814c259a7bd8264747c2e5ea1e5639ca70e85
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-10 05:43:47 +0530

    Day 04- Number Guessing

---
+++ Day-04 NumberGuessing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int random =  new Random().Next(1, 101);
+
+            //Console.WriteLine("Enter a number to Guess:");
+            //int guess = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number to Guess:");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == random)
+                {
+                    Console.WriteLine("you guessed it Right, Jackpot!");
+                    break;
+                }
+                else if(guess < random)
+                {
+                    Console.WriteLine("too low");
+                }
+                else if(guess > random)
+                {
+                    Console.WriteLine("too high");
+                }
+            }
+        }
+    }
+}
