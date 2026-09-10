37f774ca5ace03d77562fd4ea1049c843b83ec37
Karthikeyan <muthuvelkarthi5554@gmail.com>
2026-09-10 05:57:01 +0530

    Day 04- Multiplication Tables

---
+++ Day 04- Multiplication Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for Multiplication Table");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            for(int i = 1; i <= 10; i++)
+            {
+                int result = num * i;
+                Console.WriteLine($"{num} X {i} = {result}");
+            }
+            
+        }
+    }
+}
